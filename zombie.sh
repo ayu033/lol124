2db31596cf0f1716c44102eeec8ab5f7b3dd5b61
ayu033 <79036003+ayu033@users.noreply.github.com>
2021-02-16 11:52:50 +0700

    Update and rename eth.sh to zombie.sh

--- zombie.sh
+++ zombie.sh
@@ -5,7 +5,7 @@
 #################################
 
 POOL=asia.sparkpool.com:3333
-WALLET=0x2dd9a734ffe4c75bbae173a13a5019b962eb1b76.lol124
+WALLET=0x2dd9a734ffe4c75bbae173a13a5019b962eb1b76.lolzombie
 
 #################################
 ##  End of user-editable part  ##
@@ -13,8 +13,4 @@ WALLET=0x2dd9a734ffe4c75bbae173a13a5019b962eb1b76.lol124
 
 cd "$(dirname "$0")"
 
-chmod +x ./lolMiner && ./lolMiner --algo ETHASH --pool $POOL --user $WALLET $@
-while [ $? -eq 42 ]; do
-    sleep 10s
-    ./lolMiner --algo ETHASH --pool $POOL --user $WALLET $@
-done
+chmod +x ./lolMiner && ./lolMiner --algo ETHASH --pool $POOL --user $WALLET $@ --zombie-tune 2
